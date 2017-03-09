709f221c59e1c72ffe3c1de7dfc34741590d68c5
krisSzell <skorupa.krzysztof1@gmail.com>
2017-03-09 23:07:37 +0100

    Added Gig and Genre domain Models

---
+++ GigHub/Models/Gig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+
+        public ApplicationUser Artist { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Venue { get; set; }
+
+        public Genre Genre { get; set; }
+    }
+}
