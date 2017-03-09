caee94cdda8efaa7ff76c738b9eee9f4ed024e0a
krisSzell <skorupa.krzysztof1@gmail.com>
2017-03-09 23:13:35 +0100

    Overriden conventions for Gigs and Genres and updated database

--- GigHub/Models/Gig.cs
+++ GigHub/Models/Gig.cs
@@ -7,14 +7,16 @@ namespace GigHub.Models
     {
         public int Id { get; set; }
 
+        [Required]
         public ApplicationUser Artist { get; set; }
 
         public DateTime DateTime { get; set; }
 
         [Required]
-        [MaxLength(150)]
+        [StringLength(255)]
         public string Venue { get; set; }
 
+        [Required]
         public Genre Genre { get; set; }
     }
 }
